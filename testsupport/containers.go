abbac1904c4139868a095c22553d470d95d90229
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2023-08-20 06:21:04 +0530

    Update postgres to 15.4

--- testsupport/containers.go
+++ testsupport/containers.go
@@ -10,7 +10,7 @@ import (
 	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-const postgresImage = "postgres:15.3-alpine"
+const postgresImage = "postgres:15.4-alpine"
 const postgresPort = "5432"
 const postgresUserName = "postgres"
 const postgresPassword = "postgres"
